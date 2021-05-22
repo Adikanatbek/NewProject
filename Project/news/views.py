476d3d71106e716dcc0b1fd0d04cc9e6f5e6b58b
Kanatbekov <adikanatbekov5@gmail.com>
2021-05-22 12:52:18 +0600

    NewProject

---
+++ Project/news/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from news.models import News
+
+
+def index(request):
+    news = News.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, template_name='news/index.html', context=context)
+
+
+
